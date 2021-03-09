488636ba7bcf3739376d143867ff9013547813d5
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2021-03-09 22:06:05 +0800

    Update __main__.py

--- kaga/__main__.py
+++ kaga/__main__.py
@@ -34,6 +34,8 @@ from kaga import (
     LOGGER,
     BLACKLIST_CHATS,
     WHITELIST_CHATS,
+    pbot,
+    telethn,
 )
 
 # needed to dynamically load modules
@@ -814,8 +816,18 @@ def main():
             )
 
     updater.idle()
+    
+    if len(argv) not in (1, 3, 4):
+        telethn.disconnect()
+    else:
+        telethn.run_until_disconnected()
+
+    updater.idle()
 
 
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
+    telethn.start(bot_token=TOKEN)
+    pbot.start()
     main()
+    run()
