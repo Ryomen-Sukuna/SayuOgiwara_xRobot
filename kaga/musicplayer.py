bbcdd18ea65620d92032a3ad7cd58cd4187a6db9
Hayaka Ryu <muhnurfauzan73@gmail.com>
2021-04-11 10:03:06 +0800

    Create musicplayer.py

---
+++ kaga/musicplayer.py
@@ -0,0 +1,15 @@
+__mod_name__ = "Music Player"
+__help__ = """
+<b> VC PLAYER </b>
+Pemutar VC memainkan musik dalam Voice Chat grup Anda
+<b> Cara Pakai </b>
+1) ambahkan @KagaMusik ke grup Anda
+2) Jadikan sebagai admin bot
+3) Mulailah obrolan suara
+<b> Perintah </b>
+- /play [tautan/balasan: file mp3 atau tautan yt]: Putar/Tambahkan ke antrean lagu yang diberikan
+- /pause : Jeda pemutaran musik (khusus Admin)
+- /skip : Lewati lagu saat ini (khusus Admin)
+- /end : Akhiri pemutaran musik (khusus Admin)
+<b> HARAP DIPERHATIKAN, INI SANGAT TIDAK STABIL DAN DAPAT DIHENTIKAN KAPAN SAJA </b>
+"""
